ba9cf8d7e9aa2977b7a5cdfa5409368c1239707b
Funkymartians <mafersaenz2002@gmail.com>
2023-09-08 16:18:55 -0500

    yeehaw2

--- script.js
+++ script.js
@@ -5,16 +5,23 @@ document.addEventListener("DOMContentLoaded", function() {
     const moon = document.getElementById("moon");
     const textBox = document.getElementById("text-box");
   
+    // Start the sun animation as the page loads
+    sun.style.animation = "moveUp 5s infinite";
+  
     toggleButton.addEventListener("click", function() {
       if (body.classList.contains("night")) {
         body.classList.remove("night");
         sun.style.animation = "moveUp 5s infinite";
+        sun.style.opacity = "1";  // Make the sun appear
         moon.style.animation = "none";
+        moon.style.opacity = "0";  // Make the moon disappear
         textBox.style.color = "black";
       } else {
         body.classList.add("night");
         sun.style.animation = "none";
+        sun.style.opacity = "0";  // Make the sun disappear
         moon.style.animation = "moveDown 5s infinite";
+        moon.style.opacity = "1";  // Make the moon appear
         textBox.style.color = "white";
       }
     });
